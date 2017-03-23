6b81d3f5a937ab23b8da4f9deeae6d29bb76e3b9
lyf <dafengniao>
2017-03-23 18:13:14 +0800

    login

--- config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  API_ROOT: '"http://t.taihuoniao.com/app/w_api/"'
-}
--- src/store/mutation-types.js
+++
@@ -1,2 +0,0 @@
-export const USER_SIGNIN = 'USER_SIGNIN'; // 登陆
-export const USER_SIGNOUT = 'USER_SIGNOUT'; // 退出
